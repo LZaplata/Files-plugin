59447a8da0faec8671ed1dfb4d37f6d5d4c11830
Lukáš Záplata <lukas.zaplata@gmail.com>
2022-12-21 17:56:57 +0100

    Fix files filtering by categories

--- models/File.php
+++ models/File.php
@@ -1,6 +1,8 @@
 <?php namespace LZaplata\Files\Models;
 
 use Model;
+use October\Rain\Database\Builder;
+use October\Rain\Database\Scopes\NestedTreeScope;
 use October\Rain\Database\Traits\NestedTree;
 use System\Models\File as SystemFile;
 
@@ -41,4 +43,16 @@ class File extends Model
             "table" => "lzaplata_files_files_categories",
         ],
     ];
+
+    /**
+     * @param Builder $query
+     * @param array $categories
+     * @return Builder
+     */
+    public function scopeFilterCategories(Builder $query, array $categories): Builder
+    {
+        return $query->whereHas("categories", function($q) use ($categories) {
+            $q->withoutGlobalScope(NestedTreeScope::class)->whereIn("id", $categories);
+        });
+    }
 }
